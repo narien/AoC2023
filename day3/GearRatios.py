cfd03e42bcbee6e4463d27955bf42b7c2a1cafa4
Jakob Svemar <bgm@qlik.com>
2023-12-04 13:56:14 +0100

    add day3 part 2 solution

--- day3/GearRatios.py
+++ day3/GearRatios.py
@@ -1,6 +1,20 @@
 
+gearRatios = {}
+def addToGearRatios(x, y, val):
+    if (x, y) in gearRatios:
+        gearRatios[(x, y)].append(val)
+    else:
+        gearRatios[(x, y)] = [val]
 
-def isPart(schematic, schematicLine, start, end):
+def sumAllGearRatios():
+    sumOfGearRatios = 0
+    for l in gearRatios.values():
+        if len(l) == 2:
+            sumOfGearRatios += l[0] * l[1]
+    return sumOfGearRatios
+
+
+def isPart(schematic, schematicLine, start, end, val):
     start = max(0, start - 1)
     end = min(end, len(schematic[0]) - 1)
 
@@ -9,15 +23,24 @@ def isPart(schematic, schematicLine, start, end):
     above = schematic[schematicLine - 1][start:end + 1] if schematicLine > 0 else ''
     below = schematic[schematicLine + 1][start:end + 1] if schematicLine < len(schematic) - 1 else ''
     if not infront.isdigit() and not infront == '.':
+        if infront == '*':
+            addToGearRatios(start, schematicLine, val)
         return True
     if not behind.isdigit() and not behind == '.':
+        if behind == '*':
+            addToGearRatios(end, schematicLine, val)
         return True
-    for c in above:
+    for i, c in enumerate(above):
         if not c.isdigit() and not c == '.':
+            if c == '*':
+                addToGearRatios(i + start, schematicLine - 1, val)
             return True
-    for c in below:
+    for i, c in enumerate(below):
         if not c.isdigit() and not c == '.':
+            if c == '*':
+                addToGearRatios(i + start, schematicLine + 1, val)
             return True
+    return False
 
 def sumAllPartNumbers(schematic):
     sumOfParts = 0
@@ -32,8 +55,9 @@ def sumAllPartNumbers(schematic):
                     j += 1
                 end = j
 
-                if isPart(schematic, i, start, end):
-                    sumOfParts += int(schematic[i][start:end])
+                val = int(schematic[i][start:end])
+                if isPart(schematic, i, start, end, val):
+                    sumOfParts += val
             j += 1
     return sumOfParts
 
@@ -43,3 +67,5 @@ if __name__ == '__main__':
         schematic = [val.strip() for val in f]
 
     print('Sum of all parts numbers: ' + str(sumAllPartNumbers(schematic)))
+    print('Sum of all gear ratios: ' + str(sumAllGearRatios()))
+
