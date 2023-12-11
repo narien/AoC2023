936fb43271f1192821ee9bde10e8988eaf68c48d
Jakob Svemar <bgm@qlik.com>
2023-12-11 13:00:44 +0100

    add day 11 part 2 solution

--- day11/CosmicExpansion.py
+++ day11/CosmicExpansion.py
@@ -1,25 +1,3 @@
-def expandCosmos(cosmos):
-    horizontalExpands = []
-    for y in range(len(cosmos)):
-        if '#' not in cosmos[y]:
-            horizontalExpands.append(y)
-    horizontalExpands.reverse()
-    for y in horizontalExpands:
-        cosmos.insert(y, cosmos[y])
-
-    verticalExpands = []
-    for x in range(len(cosmos[0])):
-        insert = True
-        for y in cosmos:
-            if y[x] == '#':
-                insert = False
-                break
-        if insert: verticalExpands.append(x)
-    verticalExpands.reverse()
-    for x in verticalExpands:
-        for y in range(len(cosmos)):
-            cosmos[y] = cosmos[y][0:x] + '.' + cosmos[y][x:]
-
 def findGalaxies(cosmos):
     galaxies = set()
     for y in range(len(cosmos)):
@@ -28,18 +6,60 @@ def findGalaxies(cosmos):
                 galaxies.add((y, x))
     return galaxies
 
-def calcShortestPathCheckSum(galaxies, cosmos):
+def applyHorizontalDrift(cosmos, galaxies, drift):
+    horizontalExpands = []
+    newGalaxies = set()
+
+    for y in range(len(cosmos)):
+        if '#' not in cosmos[y]:
+            horizontalExpands.append(y)
+    horizontalExpands.reverse()
+    for galaxy in galaxies:
+        for y in horizontalExpands:
+            if galaxy[0] > y:
+                galaxy = (galaxy[0] + drift, galaxy[1])
+        newGalaxies.add(galaxy)
+    return newGalaxies
+
+def applyVerticalDrift(cosmos, galaxies, drift):
+    verticalExpands = []
+    newGalaxies = set()
+
+    for x in range(len(cosmos[0])):
+        insert = True
+        for y in cosmos:
+            if y[x] == '#':
+                insert = False
+                break
+        if insert: verticalExpands.append(x)
+    verticalExpands.reverse()
+    for galaxy in galaxies:
+        for x in verticalExpands:
+            if galaxy[1] > x:
+                galaxy = (galaxy[0], galaxy[1] + drift)
+        newGalaxies.add(galaxy)
+    return newGalaxies
+
+def applyGalaxyDrift(cosmos, galaxies, drift):
+    galaxies = applyHorizontalDrift(cosmos, galaxies, drift)
+    galaxies = applyVerticalDrift(cosmos, galaxies, drift)
+    return galaxies
+
+def calcShortestPathCheckSum(galaxies):
     checksum = 0
     galaxy = galaxies.pop()
     while len(galaxies):
         for otherGalaxy in galaxies:
-            checksum += abs(galaxy[0] - otherGalaxy[0]) + abs(galaxy[1] - otherGalaxy[1])
+            checksum += abs(otherGalaxy[0] - galaxy[0]) + abs(otherGalaxy[1] - galaxy[1])
         galaxy = galaxies.pop()
     return checksum
 
 if __name__ == '__main__':
     with open('day11/input.txt') as f:
         cosmos = [line.strip() for line in f]
-    expandCosmos(cosmos)
     galaxies = findGalaxies(cosmos)
-    print('Checksum for shortest path between galaxies is: ' + str(calcShortestPathCheckSum(galaxies, cosmos)))
+    galaxies = applyGalaxyDrift(cosmos, galaxies, 1)
+    print('Checksum for shortest path between galaxies is: ' + str(calcShortestPathCheckSum(galaxies)))
+    galaxiesP2 = findGalaxies(cosmos)
+    galaxiesP2 = applyGalaxyDrift(cosmos, galaxiesP2, 999999)
+    print('Checksum for shortest path between galaxies part 2 is: ' + str(calcShortestPathCheckSum(galaxiesP2)))
