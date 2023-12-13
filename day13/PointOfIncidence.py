7c29bf4903f59db0d1b061203927d8e5e44b83f0
Jakob Svemar <bgm@qlik.com>
2023-12-13 12:30:14 +0100

    rename stuff in day13

--- day13/PointOfIncidence.py
+++ day13/PointOfIncidence.py
@@ -22,39 +22,39 @@ def checkVertical(pattern, invalidRow):
         return found, preceedingRows
     return False, 0
 
-def checkPattern(pattern, csMem):
-    found, preceedingRows = checkHorizontal(pattern, csMem[1] if csMem[0] == 'h' else -1)
+def checkPattern(pattern, p1Reflection):
+    found, preceedingRows = checkHorizontal(pattern, p1Reflection[1] if p1Reflection[0] == 'h' else -1)
     if found:
         return found, preceedingRows * 100, ('h', preceedingRows - 1)
-    found, preceedingColumns = checkVertical(pattern, csMem[1] if csMem[0] == 'v' else -1)
+    found, preceedingColumns = checkVertical(pattern, p1Reflection[1] if p1Reflection[0] == 'v' else -1)
     return found, preceedingColumns, ('v', preceedingColumns - 1)
 
 
 def countPreceedingRowsAndColumns(patterns):
     checkSum = 0
-    csList = []
+    p1ReflectionList = []
     for pattern in patterns:
-        _, patternCheckSum, csMem = checkPattern(pattern, ('', -1))
+        _, patternCheckSum, p1Reflection = checkPattern(pattern, ('', -1))
         checkSum += patternCheckSum
-        csList.append(csMem)
-    return checkSum, csList
+        p1ReflectionList.append(p1Reflection)
+    return checkSum, p1ReflectionList
 
-def fixSmudgeAndCalc(pattern, csList):
-    csMem = csList.pop(0)
+def fixSmudgeAndCalc(pattern, p1ReflectionList):
+    p1Reflection = p1ReflectionList.pop(0)
     for i in range(len(pattern)):
         for j in range(len(pattern[i])):
             patternCopy = copy.deepcopy(pattern)
             patternCopy[i][j] = '.' if patternCopy[i][j] == '#' else '#'
-            found, checkSum, _ = checkPattern(patternCopy, csMem)
+            found, checkSum, _ = checkPattern(patternCopy, p1Reflection)
             if found:
                 return checkSum
     return 0
 
 
-def countPreceedingRowsAndColumnsWithSmudges(patterns, csList):
+def countPreceedingRowsAndColumnsWithSmudges(patterns, p1ReflectionList):
     checkSum = 0
     for pattern in patterns:
-        checkSum += fixSmudgeAndCalc(pattern, csList)
+        checkSum += fixSmudgeAndCalc(pattern, p1ReflectionList)
     return checkSum
 
 
@@ -71,6 +71,6 @@ if __name__ == '__main__':
             pattern.append([c for c in line])
     patterns.append(pattern)
 
-    checkSum, csList = countPreceedingRowsAndColumns(patterns)
+    checkSum, p1ReflectionList = countPreceedingRowsAndColumns(patterns)
     print('Checksum for part 1: ' + str(checkSum))
-    print('Checksum for part 2: ' + str(countPreceedingRowsAndColumnsWithSmudges(patterns, csList)))
+    print('Checksum for part 2: ' + str(countPreceedingRowsAndColumnsWithSmudges(patterns, p1ReflectionList)))
